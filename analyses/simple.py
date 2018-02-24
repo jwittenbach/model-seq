3a707f59c9257ee1b516d7c7a89eab1cd282b46c
Jason Wittenbach <json.wittenbach@gmail.com>
2018-02-23 21:53:23 -0500

    model tweaks

--- analyses/simple.py
+++ analyses/simple.py
@@ -1,6 +1,7 @@
 import logging
 
 import numpy as np
+from scipy.io import mmread
 import tensorflow as tf
 
 from modelseq.models.simple_model import SimpleModel
@@ -11,38 +12,42 @@ log_format = "%(levelname)s:%(name)s:%(message)s"
 # logging.basicConfig(format=log_format, level=logging.INFO)
 logging.basicConfig(format=log_format, level=logging.DEBUG)
 
-log_path = "logs/simple/"
+log_path = "logs/simple-10/"
 
 # data
 logger.info("loading data...")
 
-# data_path = "/Users/jason/Documents/matrix.mtx"
-# data = mmread(path).toarray().T
+#data_path = "/Users/jason/Documents/matrix.mtx"
+data_path = "/home/jason/Documents/matrix.mtx"
+data = mmread(data_path).toarray().T
 
-data_path = "/Users/jason/Documents/sampled.npy"
-data = np.load(data_path)
+#data_path = "/Users/jason/Documents/sampled.npy"
+#data_path = "/home/jason/Documents/sampled.npy"
+#data = np.load(data_path)
 
 logger.info("data shape: {}".format(data.shape))
 
 # model
 logger.info("specifying model...")
 
-model = SimpleModel(shape=data.shape, k=5, alpha=0.01)
+model = SimpleModel(shape=data.shape, k=10, alpha=0.00)
 
-# mean_counts_per_cell = data.sum(axis=1).mean()
-# mu_init = np.tile(mean_counts_per_cell, data.shape[0])[:, np.newaxis]
-# model.set_parameter('mu', mu_init)
+# mean_counts = data.mean()
+# model.set_parameter('mu', mean_counts)
+
+#mean_counts_per_gene = data.mean(axis=0)[np.newaxis, :]
+#model.set_parameter('mu_g', np.log(mean_counts_per_gene))
 
 # training details
 logger.info("setting up training...")
 
-opt = tf.train.AdamOptimizer()
+opt = tf.train.AdamOptimizer(0.1)
 
 train = Trainer(
     model=model, data=data, optimizer=opt, cv_frac=0.1, batch_frac=0.9, logdir=log_path
 )
 
-epochs = 100000
+epochs = 10000000
 n_steps = epochs * train.n_batches
 logger.info("epochs:\t{}".format(epochs))
 logger.info("steps:\t{}".format(n_steps))
@@ -56,14 +61,24 @@ logger.debug("C max:\t{}".format(vars['C'].max()))
 logger.debug("C min:\t{}".format(vars['C'].min()))
 logger.debug("G max:\t{}".format(vars['G'].max()))
 logger.debug("G min:\t{}".format(vars['G'].min()))
-X = np.dot(vars['C'], vars['G'])
-logger.debug("X max:\t{}".format(X.max()))
-logger.debug("X min:\t{}".format(X.min()))
+#logger.debug("mu_c max:\t{}".format(vars["mu_c"].max()))
+#logger.debug("mu_c min:\t{}".format(vars["mu_c"].min()))
+#logger.debug("mu_g max:\t{}".format(vars["mu_g"].max()))
+#logger.debug("mu_c min:\t{}".format(vars["mu_c"].min()))
+#logger.debug("mu:\t{}".format(vars["mu"]))
+#X = vars["mu"] + vars["mu_c"] + vars["mu_g"] + np.dot(vars['C'], vars['G'])
+# logger.debug("X max:\t{}".format(X.max()))
+# logger.debug("X min:\t{}".format(X.min()))
 for i in range(n_steps):
-    if i % 10 == 0:
+    if i % 100000 == 0:
         print("step:\t{}".format(i))
         train.step()
+#        X_tensor = tf.get_default_graph().get_tensor_by_name('X:0')
+#        X = model.session.run(X_tensor)
+#        print("X max:\t{}".format(X.max()))
+#        print("X min:\t{}".format(X.min()))
+#        print("mu:\t{}".format(model.parameters["mu"]))
         train.summarize(i, variables=True)
 
 model.save(log_path)
-logger.debug(model.parameters)
+#logger.debug(model.parameters)
--- modelseq/models/simple_model.py
+++ modelseq/models/simple_model.py
@@ -19,33 +19,33 @@ class SimpleModel(MLFactorModel):
         """
 
         # low-rank factorization
-        C = tf.Variable(tf.random_uniform((self.shape[0], self.k)), name="C")
-        G = tf.Variable(tf.random_uniform((self.k, self.shape[1])), name="G")
-        # mu = tf.Variable(tf.random_uniform((self.shape[0], 1)), name='mu')
-        # mu_rep = tf.tile(mu, (1, self.shape[1]))
+        C = tf.Variable(tf.random_uniform((self.shape[0], self.k)) - 0.5, name="C")
+        G = tf.Variable(tf.random_uniform((self.k, self.shape[1])) - 0.5, name="G")
+        #mu = tf.Variable(tf.zeros((1,))[0], name='mu')
+        mu_c = tf.Variable(tf.zeros((self.shape[0], 1)), name='mu_c')
+        mu_g = tf.Variable(tf.zeros((1, self.shape[1])), name='mu_g')
 
-        X = tf.matmul(C, G, name="X")
+        X = mu_g + mu_c + tf.matmul(C, G, name="X")
 
         # extract matrix elements for mini-batch
         X_sample = tf.boolean_mask(X, self.batch_mask)
-        # mu_sample = tf.boolean_mask(mu_rep, self.batch_mask)
-        # n_samples = tf.shape(X_sample)[0]
+        n_samples = tf.shape(X_sample)[0]
 
         # simple non-linearity
         M_sample = tf.exp(X_sample)
 
         # per-element categorical variables with probs [p, 1-p]
-        # p = tf.sigmoid(tf.Variable(tf.random_uniform((1,))[0]), name="p")
-        # probs = tf.tile([1 - p, p], (n_samples,))
-        # probs = tf.reshape(probs, (-1, 2))
-        # cat = Categorical(probs=probs)
+        p = tf.sigmoid(tf.Variable(tf.random_uniform((1,))[0]), name="p")
+        probs = tf.tile([1 - p, p], (n_samples,))
+        probs = tf.reshape(probs, (-1, 2))
+        cat = Categorical(probs=probs)
 
         # counts are mixture model between Poisson(M) (signal) and 0 (dropout)
-        # signal = Poisson(M_sample)
-        # dropout = Deterministic(0.0*tf.ones((n_samples,)))
-        # counts = Mixture(cat, [signal, dropout])
+        signal = Poisson(M_sample)
+        dropout = Deterministic(0.0*tf.ones((n_samples,)))
+        counts = Mixture(cat, [signal, dropout])
 
-        counts = Poisson(M_sample)
+        #counts = Poisson(M_sample)
         
         return counts
 
@@ -55,4 +55,9 @@ class SimpleModel(MLFactorModel):
         """
         Override
         """
-        return self.ml_loss + self.alpha * tf.norm(self.get_variables()['C'])
+        vars = self.get_variables()
+        norm_C = tf.norm(vars['C'])
+        norm_G = tf.norm(vars['G'])
+        #norm_mu_c = tf.norm(vars['mu_c'])
+        norm_mu_g = tf.norm(vars['mu_g'])
+        return self.ml_loss + self.alpha * (norm_C + norm_G)
